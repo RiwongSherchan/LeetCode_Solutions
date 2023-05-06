7d58cbc42c8e07cd284931ac7f05abcea20c8124
Riwong Sherchan <sherchanriwong@gmail.com>
2023-05-06 20:51:55 +0545

    new file: Add Strings

---
+++ LeetCode_Solutions/src/math/AddStrings.java
@@ -0,0 +1,23 @@
+package math;
+
+import java.math.BigDecimal;
+
+public class AddStrings {
+	// testing function
+	public static void main(String[] args) {
+
+		String c = addStrings("11", "44");
+		System.out.println(c);
+	}
+
+	// solution function
+	public static String addStrings(String num1, String num2) {
+		BigDecimal number1 = new BigDecimal(num1);
+		BigDecimal number2 = new BigDecimal(num2);
+		BigDecimal sum;
+		sum = number1.add(number2);
+		String result = sum.toString();
+		return result;
+
+	}
+}
